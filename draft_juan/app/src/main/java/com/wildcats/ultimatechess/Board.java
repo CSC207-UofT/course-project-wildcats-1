c6ccc36d27550b1698da56b11d7497c6b1eb35fa
guobryan <bryan.guo@mail.utoronto.ca>
2021-10-30 23:25:19 -0400

    Completed methods in Board class: addPiece removePiece movePiece checkSquareEmpty
    Helper methods:
    getRowNum
    getColNum

--- draft_juan/app/src/main/java/com/wildcats/ultimatechess/Board.java
+++ draft_juan/app/src/main/java/com/wildcats/ultimatechess/Board.java
@@ -16,8 +16,78 @@ public class Board {
      */
     public void addPiece(Piece piece, String squareID){
 
+        int rowNum = this.getRowNum(squareID);
+        int colNum = this.getColNum(squareID);
+
+        this.board[colNum][rowNum] = piece;
+    }
+
+    /**
+     * Removes a piece on the board at a specified square.
+     *
+     * Precondition: The square is not empty
+     *
+     * @param squareID The string name of the square in which a piece is removed
+     */
+    public void removePiece(String squareID){
+
+        int rowNum = this.getRowNum(squareID);
+        int colNum = this.getColNum(squareID);
+
+        this.board[colNum][rowNum] = null;
+    }
+
+    /**
+     * Moves a piece at a given location to another location.
+     *
+     * Preconditions:
+     * - There is a Piece at the start location
+     * - The end location is empty
+     *
+     * @param start The string name of the square containing a piece to be moved
+     * @param end   The string name of the square to which the piece will be moved
+     */
+    public void movePiece(String start, String end){
+        int startRowNum = this.getRowNum(start);
+        int startColNum = this.getColNum(start);
+        int endRowNum = this.getRowNum(end);
+        int endColNum = this.getColNum(end);
+
+        board[endColNum][endRowNum] = board[startColNum][startRowNum];
+        board[startColNum][startRowNum] = null;
+
+    }
+
+    /**
+     *
+     * @param squareID The String representation of the square being checked
+     * @return True if the square is empty and False otherwise
+     */
+    public boolean checkSquareEmpty(String squareID){
+
+        int rowNum = this.getRowNum(squareID);
+        int colNum = this.getColNum(squareID);
+
+        return this.board[colNum][rowNum] == null;
+    }
+
+    /**
+     * Returns the row number of the square as an int that can be used in board methods
+     *
+     * @param squareID The String representation of the square
+     */
+    private int getRowNum(String squareID) {
+
         int rowNum = squareID.substring(1) - 1;
-        int colNum;
+        return rowNum;
+    }
+
+    /**
+     * Returns the column number of the square as an int that can be used in board methods
+     *
+     * @param squareID The String representation of the square
+     */
+    private int getColNum(String squareID) {
 
         String colLetter = squareID.substring(0, 1);
         if (colLetter == "a") {
@@ -37,20 +107,6 @@ public class Board {
         } else if(colLetter == "h") {
             colNum = 7;
         }
-
-        this.board[colNum][rowNum] = piece;
-    }
-
-    /**
-     * Moves a piece at a given square to another square.
-     *
-     * @param start The string name of the square containing a piece to be moved
-     * @param end   The string name of the square to which the piece will be moved
-     */
-    public void movePiece(String start, String end){
-        //TODO Implement
-    }
-    public boolean checkSquare(String squareID){
-
+        return colNum;
     }
 }
