662fa85a9956e3169d2ec74d9846783b2996bee0
jaren <jaren.worme@mail.utoronto.ca>
2021-11-24 20:07:25 -0500

    Created Clock class

---
+++ app/src/main/java/Entities/Clock.java
@@ -0,0 +1,14 @@
+package Entities;
+
+
+public class Clock {
+    private int hours;
+    private int mins;
+    private int secs;
+
+    public Clock(int hours, int mins, int secs){
+        this.hours = hours;
+        this.mins = mins;
+        this.secs = secs;
+    }
+}
