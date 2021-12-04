f996fd288368477433b990bc533aecdef7c867fd
Juan De Agüero <juanmgdeaguero@gmail.com>
2021-12-04 15:01:48 -0500

    Base logic of LocalDatabase.

--- app/src/main/java/Interfaces/LocalDatabase.java
+++ app/src/main/java/Interfaces/LocalDatabase.java
@@ -1,29 +1,33 @@
 package Interfaces;
 
-import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 
 public class LocalDatabase {
 
+    SQLiteDatabase db;
+
     public LocalDatabase() {
-
-        SQLiteDatabase mydatabase = SQLiteDatabase.openOrCreateDatabase("login", null);
-
+        db = SQLiteDatabase.openOrCreateDatabase("login", null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS Login(Username VARCHAR, Password VARCHAR);");
     }
 
     public boolean loginInfoSaved() {
-
-        return false;
+        if (DatabaseUtils.queryNumEntries(db, "Login") == 0) return false;
+        else return true;
     }
 
     public void saveLoginInfo(String name, String password) {
-
+        db.execSQL("INSERT INTO TutorialsPoint VALUES('" + name + "','" + password + "');");
     }
 
     public String[] getLoginInfo() {
+        Cursor cursor = db.rawQuery("Select * from Login", null);
+        cursor.moveToFirst();
         String[] loginInfo = new String[2];
-        loginInfo[0] = "";
-        loginInfo[1] = "";
+        loginInfo[0] = cursor.getString(0);
+        loginInfo[1] = cursor.getString(1);
         return loginInfo;
     }
 
