e7a60765401b4726137a636c0aa162a3d6ade8f0
Juan De Agüero <juanmgdeaguero@gmail.com>
2021-12-04 14:42:46 -0500

    Start working on LocalDatabase.

--- app/src/main/java/Interfaces/LocalDatabase.java
+++ app/src/main/java/Interfaces/LocalDatabase.java
@@ -1,4 +1,30 @@
 package Interfaces;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 public class LocalDatabase {
+
+    public LocalDatabase() {
+
+        SQLiteDatabase mydatabase = SQLiteDatabase.openOrCreateDatabase("login", null);
+
+    }
+
+    public boolean loginInfoSaved() {
+
+        return false;
+    }
+
+    public void saveLoginInfo(String name, String password) {
+
+    }
+
+    public String[] getLoginInfo() {
+        String[] loginInfo = new String[2];
+        loginInfo[0] = "";
+        loginInfo[1] = "";
+        return loginInfo;
+    }
+
 }
