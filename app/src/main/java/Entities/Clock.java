a5c8a63077c897412e43c34dab4635355e0fd2b8
jaren <jaren.worme@mail.utoronto.ca>
2021-12-08 16:25:04 -0500

    Some clock formatting

--- app/src/main/java/Entities/Clock.java
+++ app/src/main/java/Entities/Clock.java
@@ -4,11 +4,12 @@ public class Clock {
 
     public Clock(){}
 
-
+    //initiate clock state
     public String getStartTime(){
         return "0 : 0 : 0";
     }
 
+    //convert time info to string in format to be shown on game window
     public String clockStringConverter(int hours, int mins, int secs){
 
         StringBuilder s = new StringBuilder("");
